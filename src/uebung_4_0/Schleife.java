66bfea65773f74e28f3147fcc067605f0c65beee
MankeT <tim.manke@nordakademie.de>
2015-11-03 11:02:57 +0100

    Fertigstellung Aufgabe 4

---
+++ src/uebung_4_0/Schleife.java
@@ -0,0 +1,14 @@
+package uebung_4_0;
+
+public class Schleife {
+
+	public static void main(String[] args) {
+		int maximum = 100;
+		int summe = 0;
+		
+		for (int zahl = 1; summe <= maximum ; zahl = zahl + 1)
+		{ System.out.println(summe); 
+		summe = summe + zahl;}
+	}
+
+}
